0dc9f5be56310d507a5bcf04badb0c62908d9a08
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2022-09-28 05:44:47 +0200

    next n prev btns working

--- tables.js
+++ tables.js
@@ -35,6 +35,7 @@ async function createTable() {
 
     document.addEventListener('click', function (e) {
         // console.log(e.target.nodeName);
+        // console.log(e.target.id);
         if (
             e.target.nodeName == 'A' &&
             e.target.classList.contains('clickPageNumber')
@@ -49,57 +50,52 @@ async function createTable() {
             e.target.classList.add('active');
             // console.log(e.target);
         }
-    });
-
-    /*
- 
-
-// Extract value from table header.
-// ('Book ID', 'Book Name', 'Category' and 'Price')
-let col = [];
-for (let i = 0; i < doctorsArray.length; i++) {
-    for (let key in doctorsArray[i]) {
-        if (col.indexOf(key) === -1) {
-            col.push(key);
+        if (e.target.nodeName == 'A' && e.target.id === 'button_prev') {
+            // console.log('prev clicked');
+            if (CURRENT_PAGE !== 1) {
+                CURRENT_PAGE--;
+                DisplayList(
+                    doctorsArray,
+                    list_element,
+                    PAGE_SIZE,
+                    CURRENT_PAGE
+                );
+                // Remove from other active
+                let current_btn = document.querySelector('.page-item a.active');
+                current_btn.classList.remove('active');
+                // Add to current page
+                document
+                    .querySelectorAll('.clickPageNumber')
+                    .forEach((item) => {
+                        if (item.innerHTML == CURRENT_PAGE) {
+                            item.classList.add('active');
+                        }
+                    });
+            }
         }
-    }
-}
-
-// Create table.
-const table = document.createElement('table');
-table.classList.add(
-    'table',
-    'table-bordered',
-    'align-middle',
-    'w-100',
-    'text-center'
-);
-table.setAttribute('id', 'pagination_table');
-
-// Create table header row using the extracted headers above.
-let tr = table.insertRow(-1); // table row.
-
-for (let i = 0; i < col.length; i++) {
-    let th = document.createElement('th'); // table header.
-    th.innerHTML = col[i];
-    tr.appendChild(th);
-}
-
-// add json data to the table as rows.
-for (let i = 0; i < doctorsArray.length; i++) {
-    tr = table.insertRow(-1);
-
-    for (let j = 0; j < col.length; j++) {
-        let tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = doctorsArray[i][col[j]];
-    }
-}
-
-// Now, add the newly created table with json data, to a container.
-const divShowData = document.getElementById('showData');
-divShowData.innerHTML = '';
-divShowData.appendChild(table);
- */
+        if (e.target.nodeName == 'A' && e.target.id === 'button_next') {
+            if (CURRENT_PAGE !== NUMBER_OF_PAGES) {
+                CURRENT_PAGE++;
+                DisplayList(
+                    doctorsArray,
+                    list_element,
+                    PAGE_SIZE,
+                    CURRENT_PAGE
+                );
+                // Remove from other active
+                let current_btn = document.querySelector('.page-item a.active');
+                current_btn.classList.remove('active');
+                // Add to current page
+                document
+                    .querySelectorAll('.clickPageNumber')
+                    .forEach((item) => {
+                        if (item.innerHTML == CURRENT_PAGE) {
+                            item.classList.add('active');
+                        }
+                    });
+            }
+        }
+    });
 
     // Pagination
     const nav = document.createElement('nav');
