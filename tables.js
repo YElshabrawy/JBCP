be1d903b34c7110b898374da17fd691451d6113f
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2022-09-28 05:08:47 +0200

    fixing js tables file

--- tables.js
+++ tables.js
@@ -1,56 +1,57 @@
 // Json data
-import doctorsArray from './data/doctors.json' assert { type: 'json' };
-// console.log(doctorsArray);
+async function createTable() {
+    const response = await fetch('./data/doctors.json');
+    const doctorsArray = await response.json();
 
-// get elements
-const list_element = document.getElementById('doctors_list');
-const paginationElement = document.getElementById('page_numbers');
+    // get elements
+    const list_element = document.getElementById('doctors_list');
+    const paginationElement = document.getElementById('page_numbers');
 
-let CURRENT_PAGE = 1;
-const PAGE_SIZE = 10;
+    let CURRENT_PAGE = 1;
+    const PAGE_SIZE = 10;
 
-// Display list
-function DisplayList(items, wraper, rows, page) {
-    wraper.innerHTML = '';
-    page--;
+    // Display list
+    function DisplayList(items, wraper, rows, page) {
+        wraper.innerHTML = '';
+        page--;
 
-    let loop_start = rows * page;
-    let paginatedItems = items.slice(loop_start, loop_start + rows);
+        let loop_start = rows * page;
+        let paginatedItems = items.slice(loop_start, loop_start + rows);
 
-    // console.log(paginatedItems);
-    for (let i = 0; i < paginatedItems.length; i++) {
-        const item = paginatedItems[i];
+        // console.log(paginatedItems);
+        for (let i = 0; i < paginatedItems.length; i++) {
+            const item = paginatedItems[i];
 
-        const tr = document.createElement('tr');
-        tr.innerHTML = `<td>${item['العنوان']}</td>
+            const tr = document.createElement('tr');
+            tr.innerHTML = `<td>${item['العنوان']}</td>
                             <td>${item['المنطقة']}</td>
                             <td>${item['رقم الهاتف']}</td>
                             <td>${item['اسم الطبيب']}</td>`;
 
-        wraper.appendChild(tr);
+            wraper.appendChild(tr);
+        }
     }
-}
-DisplayList(doctorsArray, list_element, PAGE_SIZE, CURRENT_PAGE);
+    DisplayList(doctorsArray, list_element, PAGE_SIZE, CURRENT_PAGE);
 
-document.addEventListener('click', function (e) {
-    // console.log(e.target.nodeName);
-    if (
-        e.target.nodeName == 'A' &&
-        e.target.classList.contains('clickPageNumber')
-    ) {
-        CURRENT_PAGE = e.target.textContent;
-        DisplayList(doctorsArray, list_element, PAGE_SIZE, CURRENT_PAGE);
+    document.addEventListener('click', function (e) {
+        // console.log(e.target.nodeName);
+        if (
+            e.target.nodeName == 'A' &&
+            e.target.classList.contains('clickPageNumber')
+        ) {
+            CURRENT_PAGE = e.target.textContent;
+            DisplayList(doctorsArray, list_element, PAGE_SIZE, CURRENT_PAGE);
 
-        // Remove from other active
-        let current_btn = document.querySelector('.page-item a.active');
-        current_btn.classList.remove('active');
-        // console.log('ggg', current_btn);
-        e.target.classList.add('active');
-        // console.log(e.target);
-    }
-});
+            // Remove from other active
+            let current_btn = document.querySelector('.page-item a.active');
+            current_btn.classList.remove('active');
+            // console.log('ggg', current_btn);
+            e.target.classList.add('active');
+            // console.log(e.target);
+        }
+    });
 
-/*
+    /*
  
 
 // Extract value from table header.
@@ -100,28 +101,28 @@ divShowData.innerHTML = '';
 divShowData.appendChild(table);
  */
 
-// Pagination
-const nav = document.createElement('nav');
-nav.classList.add('align-self-center');
-nav.setAttribute('aria-label', 'Page navigation example');
+    // Pagination
+    const nav = document.createElement('nav');
+    nav.classList.add('align-self-center');
+    nav.setAttribute('aria-label', 'Page navigation example');
 
-const ul = document.createElement('ul');
-ul.classList.add('pagination');
+    const ul = document.createElement('ul');
+    ul.classList.add('pagination');
 
-const calculatePagesCount = (pageSize, totalCount) => {
-    // we suppose that if we have 0 items we want 1 empty page
-    return totalCount < pageSize ? 1 : Math.ceil(totalCount / pageSize);
-};
+    const calculatePagesCount = (pageSize, totalCount) => {
+        // we suppose that if we have 0 items we want 1 empty page
+        return totalCount < pageSize ? 1 : Math.ceil(totalCount / pageSize);
+    };
 
-// const PAGE_SIZE = 10;
-const NUMBER_OF_PAGES = calculatePagesCount(PAGE_SIZE, doctorsArray.length);
+    // const PAGE_SIZE = 10;
+    const NUMBER_OF_PAGES = calculatePagesCount(PAGE_SIZE, doctorsArray.length);
 
-// console.log(NUMBER_OF_PAGES);
-// console.log(doctorsArray.length);
+    // console.log(NUMBER_OF_PAGES);
+    // console.log(doctorsArray.length);
 
-ul.insertAdjacentHTML(
-    'afterbegin',
-    `<li class="page-item">
+    ul.insertAdjacentHTML(
+        'afterbegin',
+        `<li class="page-item">
         <a
             class="page-link"
             role="button"
@@ -131,25 +132,27 @@ ul.insertAdjacentHTML(
             &laquo;
         </a>
     </li>`
-);
+    );
 
-for (let i = 1; i <= NUMBER_OF_PAGES; i++) {
-    const activeClass = i === 1 ? ' active' : '';
+    for (let i = 1; i <= NUMBER_OF_PAGES; i++) {
+        const activeClass = i === 1 ? ' active' : '';
+        ul.insertAdjacentHTML(
+            'beforeend',
+            `<li class="page-item">
+            <a class="page-link clickPageNumber${activeClass}" role="button">${i}</a>
+        </li>`
+        );
+    }
+
     ul.insertAdjacentHTML(
         'beforeend',
         `<li class="page-item">
-            <a class="page-link clickPageNumber${activeClass}" role="button">${i}</a>
-        </li>`
-    );
-}
-
-ul.insertAdjacentHTML(
-    'beforeend',
-    `<li class="page-item">
         <a class="page-link" role="button" aria-label="Next" id="button_next">
             &raquo;
         </a>
     </li>`
-);
+    );
 
-paginationElement.appendChild(ul);
+    paginationElement.appendChild(ul);
+}
+createTable();
